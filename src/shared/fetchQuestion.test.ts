391eb39d164daad485b38c2a7c7549ee32c33e5f
lisamariepleyer <Lisa-Marie Pleyer>
2023-10-07 19:29:49 +0200

    add dummy unit test

---
+++ src/shared/fetchQuestion.test.ts
@@ -0,0 +1,30 @@
+import { fetchQuestion } from './fetchQuestion';
+
+describe("Fetch Quiz tests", () => {
+    test("The fetchQuestion function can fetch a question", async () => {
+
+        // simulate XMLHttpRequest() function without actually making a network request
+        (XMLHttpRequest as any) = jest.fn().mockImplementation(() => {
+            return {
+                open: jest.fn(),
+                send: jest.fn(),
+                readyState: 4,
+                status: 200,
+                responseText: JSON.stringify({
+                    results: [{
+                        question: "Sample Question?",
+                        correct_answer: "Correct Answer",
+                            incorrect_answers: ["Wrong 1", "Wrong 2", "Correct Answer", "Wrong 3"]
+                    }]
+                })
+            };
+        });
+
+        // add tests 
+        fetchQuestion((data) => {
+            expect(data.question).toBeInstanceOf(String);
+            expect(data.answers).toHaveLength(4);
+            expect(data.answers.indexOf(data.correctAnswer) > -1);
+        });
+    });
+})
