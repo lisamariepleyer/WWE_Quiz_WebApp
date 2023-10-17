0557280fd01fe5f24ae4b24d3d92b722df04944d
lisamariepleyer <Lisa-Marie Pleyer>
2023-10-18 00:39:26 +0200

    add next question button

--- src/quiz/quiz.ts
+++ src/quiz/quiz.ts
@@ -1,38 +1,55 @@
-console.log("This is the quiz page.");
-
 import './styles.scss';
 import { fetchQuestion } from '../shared/fetchQuestion';
 import { insertMenuBar } from "../shared/menubar.ts";
 
-insertMenuBar();
+function renderQuestion(question: string) {
+    const questionElement = document.createElement('p');
+    questionElement.innerHTML = question;
+    document.body.appendChild(questionElement);
+}
+
+function renderAnswerButtons(answers: string[], correctAnswer: string) {
+    const answersContainer = document.createElement('div');
+    document.body.appendChild(answersContainer);
+
+    answers.forEach(answer => {
+
+        const answerButton = document.createElement('quizbutton');
+        answerButton.innerHTML = answer;
+        answerButton.onclick = () => {
+
+            answerButton.classList.remove('correct-answer', 'incorrect-answer');
+
+            if (answer === correctAnswer) {
+                answerButton.classList.add('correct-answer');
+                handleCorrectAnswer();
+            } else {
+                answerButton.classList.add('incorrect-answer');
+                setTimeout(() => {
+                    answerButton.classList.remove('incorrect-answer');
+                }, 3000);
+            }
+        };
+        answersContainer.appendChild(answerButton);
+    });
+}
+
+function handleCorrectAnswer() {
+    const resolveQuestionContainer = document.createElement('div');
+    document.body.appendChild(resolveQuestionContainer);
+
+    const nextQuestionButton = document.createElement('button');
+    nextQuestionButton.innerHTML = 'Next Question';
+    nextQuestionButton.onclick = () => { window.location.reload(); }
+    resolveQuestionContainer.appendChild(nextQuestionButton);
+}
 
 function init() {
+    insertMenuBar();
+
     fetchQuestion(({ question, correctAnswer, answers }) => {
-
-        const questionElement = document.createElement('p');
-        questionElement.innerHTML = question;
-        document.body.appendChild(questionElement);
-
-        answers.forEach(answer => {
-
-            const answerButton = document.createElement('quizbutton');
-            answerButton.innerHTML = answer;
-            answerButton.onclick = () => {
-
-                answerButton.classList.remove('correct-answer', 'incorrect-answer');
-
-                if (answer === correctAnswer) {
-                    answerButton.classList.add('correct-answer');
-                } else {
-                    answerButton.classList.add('incorrect-answer');
-                    setTimeout(() => {
-                        answerButton.classList.remove('incorrect-answer');
-                    }, 3000);
-                }
-            };
-            
-            document.body.appendChild(answerButton);
-        });
+        renderQuestion(question);
+        renderAnswerButtons(answers, correctAnswer);
     });
 }
 
