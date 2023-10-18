05b4adb79e795cab9968e37e767d0a98f820783c
lisamariepleyer <Lisa-Marie Pleyer>
2023-10-19 00:23:58 +0200

    handle user input data

--- src/account/account.ts
+++ src/account/account.ts
@@ -3,6 +3,11 @@ import { insertMenuBar } from "../shared/menubar.ts";
 
 function renderFavourites() {
   const favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+
+  const existingTable = document.getElementsByTagName('table');
+  if (existingTable.length > 0) {
+    existingTable[0].remove();
+  }
   const table = document.createElement('table');
 
   favourites.forEach((fav: { question: string | null; answer: string | null }) => {
@@ -16,9 +21,50 @@ function renderFavourites() {
   document.body.appendChild(table);
 }
 
+function handleUserDataInput() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('inputForm') as HTMLFormElement;
+
+    form.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (validateInputs()) {
+        renderFavourites();
+        console.log('yes');
+      } else {
+        console.log('nope');
+      }
+    });
+
+    form.addEventListener('input', validateInputs);
+  });
+}
+
+function validateInputs(): boolean {
+  const inputs = document.querySelectorAll('input[required]') as NodeListOf<HTMLInputElement>;
+  let allValid = true;
+
+  inputs.forEach(input => {
+    const errorMessage = input.parentElement?.querySelector('.error-message') as HTMLElement;
+
+    if (input.validity.valid) {
+      input.classList.add('valid');
+      input.classList.remove('invalid');
+      errorMessage.style.display = 'none';
+    } else {
+      allValid = false;
+      input.classList.add('invalid');
+      input.classList.remove('valid');
+      errorMessage.textContent = input.validationMessage;
+      errorMessage.style.display = 'block';
+    }
+  });
+
+  return allValid;
+}
+
 function init() {
   insertMenuBar();
-  renderFavourites();
+  handleUserDataInput();
 }
 
 init();
