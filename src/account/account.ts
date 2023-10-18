6af3e2fe94d11a642c539776cf157d64309b07ab
lisamariepleyer <Lisa-Marie Pleyer>
2023-10-18 22:07:45 +0200

    add favorites functionality

--- src/account/account.ts
+++ src/account/account.ts
@@ -1,6 +1,24 @@
-console.log("This is the account page.");
-
 import './styles.scss';
 import { insertMenuBar } from "../shared/menubar.ts";
 
-insertMenuBar();
+function renderFavourites() {
+  const favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+  const table = document.createElement('table');
+
+  favourites.forEach((fav: { question: string | null; answer: string | null }) => {
+    const row = table.insertRow();
+    const questionCell = row.insertCell(0);
+    const answerCell = row.insertCell(1);
+    questionCell.textContent = fav.question;
+    answerCell.textContent = fav.answer;
+  });
+
+  document.body.appendChild(table);
+}
+
+function init() {
+  insertMenuBar();
+  renderFavourites();
+}
+
+init();
